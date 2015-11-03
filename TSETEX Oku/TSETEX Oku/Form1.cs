ee4e9ce556b45714d332a44a7099251648972b1c
myrza1 <mectep@hotmail.com>
2015-11-03 10:25:22 +0600

    The Two

--- TSETEX Oku/TSETEX Oku/Form1.cs
+++ TSETEX Oku/TSETEX Oku/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,75 @@ namespace TSETEX_Oku
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool Blok;
+            string Flight = "";
+            string dayOfMonth = "";
+            string mvtType = "";
+            string aircraftRegistration = "";
+            string airport = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader("MVT.txt"))
+                {
+                    String line;
+                    Blok = false;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line == "MVT")
+                        {
+                            listBox1.Items.Add(line);
+                            Blok = true;
+                            Flight = "";
+                            mvtType = "";
+                            dayOfMonth = "";
+                            aircraftRegistration = "";
+                            airport = "";
+                        }
+                        else if (line.Length == 0)
+                        {
+                            Blok = false;
+                        }
+                        else if (Blok)
+                        {
+                            if (line.IndexOf("/") != -1 && Flight == "")
+                            {
+                                Flight = line.Substring(0, line.IndexOf("/"));
+                                dayOfMonth = line.Substring(line.IndexOf("/") + 1, 2);
+                                aircraftRegistration = line.Substring(line.IndexOf("/") + 4, (line.Length - 4) - (line.IndexOf("/") + 4));
+                                airport = line.Substring(line.Length - 3, 3);
+                                listBox1.Items.Add("No: " + Flight + " Day: " + dayOfMonth + " REG: " + aircraftRegistration + " Airport: " + airport);
+                            }
+                            else if (line.StartsWith("AA"))
+                            {
+                                mvtType = line.Substring(0, line.IndexOf("/"));
+                                
+                                listBox1.Items.Add(mvtType + " Arrival " + mvtType.Substring(0, 2));
+
+                            }
+                            else if (line.StartsWith("AD"))
+                            {
+                                mvtType = line.Substring(0, line.IndexOf("/"));
+                                listBox1.Items.Add(mvtType + " Departure " + mvtType.Substring(0, 2));
+                            }
+
+
+                            //line.Substring(0, line.IndexOf("/"));
+                            //listBox1.Items.Add(line + " " + line.IndexOf("/"));
+                        }
+
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "Couln not read file";
+            }
         }
     }
 }
